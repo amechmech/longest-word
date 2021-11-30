236d10feeb7a3ade77cc71b24853aebf713b3fe9
Aymen <aymen+training@mechmech.org>
2021-11-30 14:52:04 +0100

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -4,6 +4,7 @@
 
 import string
 import random
+import requests
 
 class Game:
     def __init__(self):
@@ -23,4 +24,10 @@ class Game:
                 #print(f'letters ongoing is {letters}')
             else:
                 return False
-        return True
+        return self.__check_dictionary(word)
+    
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
